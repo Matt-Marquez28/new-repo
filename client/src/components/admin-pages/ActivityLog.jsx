47d3972b31ef824fb132b1f6cd48523a213ed970
Matt-Marquez28 <vanmarquez999@gmail.com>
2025-01-16 14:19:53 +0800

    remove unimportant text

--- client/src/components/admin-pages/ActivityLog.jsx
+++ client/src/components/admin-pages/ActivityLog.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ActivityLog = () => {
   return (
     <div className="container">
-      <h5 className="text-primary pt-serif-bold">Activity Log</h5>
+      
     </div>
   );
 };
