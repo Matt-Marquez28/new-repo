67529d178a0c392ab7498f10889d51ab1ccfc6cf
Matt-Marquez28 <vanmarquez999@gmail.com>
2025-04-01 16:20:37 +0800

    fixed the server url

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -26,16 +26,16 @@
 //   "https://peso-8oly.onrender.com/api/v1/audit-trail";
 
   export const ACCOUNT_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/account";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/account";
 export const JOBSEEKER_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/jobseeker";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/jobseeker";
 export const COMPANY_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/company";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/company";
 export const NOTIFICATION_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/notification";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/notification";
 export const JOB_VACANCY_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/job-vacancy";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/job-vacancy";
 export const APPLICATION_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/application";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/application";
 export const AUDIT_TRAIL_API_END_POINT =
-  "https://https://peso-city-of-taguig-web-server.onrender.com/api/v1/audit-trail";
+  "https://peso-city-of-taguig-web-server.onrender.com/api/v1/audit-trail";
